ca830469681893d9bc9bfaa2d9b91030998f7980
Abbas Ali <abbas@swiftnine.com>
2026-02-27 11:57:32 +0500

    Add User feature updated

--- src/app/(admin-panel)/admin/user-management/page.tsx
+++ src/app/(admin-panel)/admin/user-management/page.tsx
@@ -188,6 +188,7 @@ export default function UserManagement() {
           name: newUserData.name,
           email: newUserData.email,
           password: newUserData.password,
+          type: "ADMIN",
           badgeId: newUserData.badgeId,
         }),
       });
